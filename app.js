0f0d7dbf11279d4c54f86f4f8b059d8ce9abcf15
moinuddin6 <114233774+moinuddin6@users.noreply.github.com>
2022-10-04 15:08:12 +0530

    Comment out

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("script is working on different files");
+// console.log("script is working on different files");
+// alert("This script is workk");
+
+// input 
+var username = prompt("Give me username");
+
+// processing 
+
+var welcomeMessage = "This script is working " + username;
+
+// output
+
+alert(welcomeMessage);
